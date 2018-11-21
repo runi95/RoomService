825305de8c9391728c96253c9c2fa9b60132886f
runi95 <rse005@uib.no>
2018-11-21 14:09:02 +0100

    Changed generation strategy to fix some persistence issues

--- src/main/java/models/Room.java
+++ src/main/java/models/Room.java
@@ -11,7 +11,7 @@ import java.time.LocalDate;
 public class Room {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     private String ownedBy;
